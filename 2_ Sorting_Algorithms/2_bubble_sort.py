9ea87474e7d08425bd81d959e1b95c4efc3889f7
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2021-12-29 23:41:20 +0600

    Update 2_bubble_sort.py

--- 2_ Sorting_Algorithms/2_bubble_sort.py
+++ 2_ Sorting_Algorithms/2_bubble_sort.py
@@ -1,3 +1,27 @@
+
+li = [1,2,3,4,5]
+#    0 1 2 3 4
+print('before sort = ',li)
+n = len(li)
+count = 0
+#outer loop
+for i in range(0,n):
+    # inner loop
+    s = 0
+    for j in range(n - 1 - i):
+        count += 1
+        if li[j] > li[j+1]:
+            # 12   3
+            li[j],li[j+1] = li[j+1],li[j]
+            s = 1
+    if s == 0:
+        break
+
+print('after sort = ',li)
+
+print('count = ',count)
+
+
 '''L = [5,3,1,4,2]
 n = len(L)
 for i in range(0,n):
@@ -17,7 +41,8 @@ if __name__ == '__main__':
     L = [3,1,7,6,2]
     bubble_sort(L)
 '''
-class Bubble_sort:
+
+'''class Bubble_sort:
     def __init__(self,L):
         self.L = L
         n = len(L)
@@ -28,8 +53,4 @@ class Bubble_sort:
         print(L)
 if __name__ == '__main__':
     L = [3,2,10,8,9,7,5,6,4,1]
-    Bubble_sort(L)
-
-
-
-
+    Bubble_sort(L)'''
